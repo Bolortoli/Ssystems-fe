6aa9b474dfb0c815007d09d7813164456b936511
bolortoli <boogiibobko@gmail.com>
2023-09-23 17:10:06 +0800

    Added pricing page

--- components/HomeTwo/Services.js
+++ components/HomeTwo/Services.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import Link from "next/link";
+import axios from "axios";
 
 export default class Services extends Component {
+
+  state = {
+    data: this.props.data || {}
+  };
+
   // Tab
   openTabSection = (evt, tabNmae) => {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabs-item");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].classList.remove("animate__fadeInUp");
+      // tabcontent[i].classList.remove("animate__fadeInUp");
       tabcontent[i].style.display = "none";
     }
 
@@ -17,84 +23,73 @@ export default class Services extends Component {
     }
 
     document.getElementById(tabNmae).style.display = "block";
-    document.getElementById(tabNmae).className +=
-      " animate__fadeInUp animate__animated";
+    // document.getElementById(tabNmae).className +=
+    //   " animate__fadeInUp animate__animated";
     evt.currentTarget.className += "current";
   };
+  componentDidUpdate(prevProps) {
+    // do something
+  }
 
   render() {
+
     return (
-      <>
+      <div key={this.state.data.detail_information}>
         <div className="services-section bg-f2f6f9 ptb-110">
           <div className="container">
             <div className="section-title">
-              <h2>We Offer Professional Solutions</h2>
+              <h2>{this.state.data.page_title}</h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                {this.state.data.page_description}
               </p>
             </div>
 
-            <div className="tab services-tab-list">
-              <div className="row">
-                <div className="col-lg-4 col-md-4">
+            <div className="tab services-tab-list card">
+              <div className="row card-body">
+                <div className="col-lg-3 col-md-3">
                   <ul className="tabs">
-                    <li
-                      className="current"
-                      onClick={(e) => this.openTabSection(e, "tab1")}
-                    >
-                      <i className="flaticon-income"></i>
-                      <span>Data Analysis</span>
-                    </li>
-
-                    <li onClick={(e) => this.openTabSection(e, "tab2")}>
-                      <i className="flaticon-automatic"></i>
-                      <span>Automation Optimization</span>
-                    </li>
-
-                    <li onClick={(e) => this.openTabSection(e, "tab3")}>
-                      <i className="flaticon-locked"></i>
-                      <span>Security & Surveillance</span>
-                    </li>
-
-                    <li onClick={(e) => this.openTabSection(e, "tab4")}>
-                      <i className="flaticon-molecular"></i>
-                      <span>Health & Manufacturing</span>
-                    </li>
-
-                    <li onClick={(e) => this.openTabSection(e, "tab5")}>
-                      <i className="flaticon-gear"></i>
-                      <span>Software Engineers</span>
-                    </li>
-
-                    <li onClick={(e) => this.openTabSection(e, "tab6")}>
-                      <i className="flaticon-ceo"></i>
-                      <span>IT Professional</span>
-                    </li>
+                    {this.state.data.detail_information.data.map((d, index) => (
+                      <li
+                        className={index == 0 ? "current" : ""}
+                        onClick={(e) => this.openTabSection(e, d.serviceName)}
+                      >
+                        {/* <i className="flaticon-income"></i> */}
+                        <span>{d.serviceName}</span>
+                      </li>
+                    ))}
                   </ul>
                 </div>
 
-                <div className="col-lg-8 col-md-8">
+                <div className="col-lg-9 col-md-9">
                   <div className="tab-content">
-                    <div id="tab1" className="tabs-item">
-                      <div className="image">
-                        <img src="/images/services-img1.png" alt="image" />
-                      </div>
-
-                      <div className="content">
-                        <p>
-                          There are many variations of passages of Lorem Ipsum
-                          available, but the majority have suffered alteration in
-                          some form, by injected humour, or randomized words which
-                          don't look even slightly believable. If you are going to
-                          use a passage.
-                        </p>
-
-                        <Link href="/service-details" className="btn btn-primary">
-                          Read More
-                        </Link>
-                      </div>
-                    </div>
+                    {this.state.data.detail_information.data.map(d => (
+                      <div id={d.serviceName} className="tabs-item">
+                        <div className="table-responsive">
+                          <table className="table table-hover table-borderless">
+                            <thead>
+                              <tr>
+                                {Object.keys(d.detailData[0]).map(key => (<th scope="col">{key}</th>))}
+                                {/* <th scope="col">Contact</th> */}
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {d.detailData.map((row) => (
+                                <>
+                                  <tr key={row.id}>
+                                    {Object.keys(row).map(key => (<td>{row[`${key}`]}</td>))}
+                                  </tr>
+                                  {/* <tr>
+                                    <Link href={`/contact`}>
+                                      <i className="flaticon-robot-1"></i>
+                                      Contact
+                                    </Link>
+                                  </tr> */}
+                                </>
+                              ))}
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>))}
 
                     <div id="tab2" className="tabs-item">
                       <div className="image">
@@ -224,7 +219,7 @@ export default class Services extends Component {
             <img src="/images/shape/dot3.png" alt="image" />
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }
--- pages/services-two.js
+++ pages/services-two.js
@@ -1,23 +1,55 @@
-import React from "react";
-import NavbarTwo from "../components/Layouts/NavbarTwo";
+import React, { useState, useEffect } from "react";
+import NavbarFour from "../components/Layouts/NavbarFour";
 import PageBanner from "../components/Common/PageBanner";
 import Services from "../components/HomeTwo/Services";
 import Footer from "../components/Layouts/Footer";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 const ServicesTwo = () => {
+
+  const [pricingData, setPricingData] = useState(null)
+  const [translatedData, setTranslatedData] = useState(null)
+
+  const { locale } = useRouter();
+
+  const fetchData = async () => {
+    const data = (await axios.get(`http://0.0.0.0:8055/items/pricing?fields=*.*.*`)).data
+    setPricingData(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    if (pricingData != null) {
+      const translationData = pricingData.data.translations.filter(d => d.languages_code.code == locale)[0]
+      setTranslatedData(translationData)
+    }
+  }, [pricingData])
+
+  useEffect(() => {
+    if (pricingData != null) {
+      console.log(locale)
+      const translationData = pricingData.data.translations.filter(d => d.languages_code.code == locale)[0]
+      setTranslatedData(translationData)
+    }
+  }, [locale])
+
   return (
     <>
-      <NavbarTwo />
+      <NavbarFour />
 
-      <PageBanner
+      {/* <PageBanner
         pageTitle="Services Two"
         homePageUrl="/"
         homePageText="Home"
         activePageText="Services Two"
         bgImgClass="item-bg2"
-      />
+      /> */}
 
-      <Services />
+      {translatedData != null ? <Services data={translatedData} /> : <></>}
 
       <Footer />
     </>
